a4aabf95836951f8da0f9a8f9acb1a9021b5dbc7
Sophia Nasseri <nasseri.sophia@gmail.com>
2022-06-09 16:34:27 +0200

    feat: debounce filter function

--- src/components/Table/GlobalFilter/GlobalFilter.jsx
+++ src/components/Table/GlobalFilter/GlobalFilter.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+import { useAsyncDebounce } from 'react-table/';
+
 const GlobalFilter = ({ filter, setFilter }) => {
+  const [value, setValue] = useState(filter);
+
+  //Debounce for better parformance
+  const onChange = useAsyncDebounce((value) => {
+    setFilter(value || undefined);
+  }, 300);
+
   return (
     <span>
       Search :{' '}
       <input
-        value={filter || ''}
-        onChange={(e) => setFilter(e.target.value)}
+        value={value || ''}
+        onChange={(e) => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
       ></input>
     </span>
   );
